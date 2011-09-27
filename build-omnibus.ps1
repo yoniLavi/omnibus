4d189453be345f0f1e6cdabd7a2ed5fa49391b60
Seth Chisamore <schisamo@opscode.com>
2011-09-27 05:09:22 -0400

    namespace all chef-solo stuff in its own directory...incase chef-client is running on the system

--- build-omnibus.ps1
+++ build-omnibus.ps1
@@ -8,7 +8,7 @@ $s3_secret_key = $args[3]
 
 Write-Output "Starting omnibus build of $project_name"
 
-New-Item C:\chef -type directory > $ENV:TEMP\omnibus.out 2>&1
+New-Item C:\chef-solo -type directory > $ENV:TEMP\omnibus.out 2>&1
 $json_attribs = @"
 {
   "aws": {
@@ -26,13 +26,13 @@ $json_attribs = @"
 }
 "@
 
-$json_attribs | Out-File -Encoding ASCII C:\chef\omnibus.json
+$json_attribs | Out-File -Encoding ASCII C:\chef-solo\omnibus.json
 
 # (FU) MS - we have to use a script block since Write-Host output doesn't go to STDERR or STDOUT
 # https://connect.microsoft.com/PowerShell/feedback/details/283088/script-logging-needs-to-be-improved
 $script = {
   Start-Process -FilePath 'git' 'pull' -WorkingDirectory "C:\omnibus" -Wait -NoNewWindow
-  Start-Process -FilePath 'chef-solo' '-c c:/chef/solo.rb -j c:/chef/omnibus.json' -Wait -NoNewWindow
+  Start-Process -FilePath 'chef-solo' '-c c:/chef-solo/solo.rb -j c:/chef-solo/omnibus.json' -Wait -NoNewWindow
 }
 
 PowerShell $script >> $ENV:TEMP\omnibus.out 2>&1
