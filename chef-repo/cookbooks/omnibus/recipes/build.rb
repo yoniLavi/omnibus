a3027f8aac4d72bdaceda9957b5173f60fd88e27
Seth Chisamore <schisamo@opscode.com>
2011-10-21 14:19:05 -0700

    don't try and use chef version to install ohai :/

--- chef-repo/cookbooks/omnibus/recipes/build.rb
+++ chef-repo/cookbooks/omnibus/recipes/build.rb
@@ -42,8 +42,8 @@ when 'windows'
   end
 
   # Ohai
+  # TODO remove this once final Ohai is released
   gem_package "ohai" do
-    version node['chef-full']['version']
     gem_binary "#{embedded_dir}\\bin\\gem"
     options "-n '#{node['chef-full']['home']}\\bin' --no-rdoc --no-ri --pre"
   end
