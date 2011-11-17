f5c34faabc20720d04d1c31981ca1b4a1f69902f
Steven Danna <steve@opscode.com>
2011-11-16 20:55:50 -0800

    Remove dependency on perl.  Manage platform_version to download correct build.

--- source/install.sh
+++ source/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-release_version="0.10.0-1"
+release_version="0.10.4-6"
 use_shell=0
 
 # Check whether a command exists - returns 0 if it does, 1 if it does not
@@ -9,14 +9,14 @@ exists() {
   then
     return 0
   else
-    return 1 
+    return 1
   fi
 }
 
 # Set the filename for a deb, based on version and machine
 deb_filename() {
   filetype="deb"
-  if [ $machine = "x86_64" ]; 
+  if [ $machine = "x86_64" ];
   then
     filename="chef-full_${version}_amd64.deb"
   else
@@ -42,58 +42,75 @@ do
   case "$opt" in
     v)  version="$OPTARG";;
     s)  use_shell=1;;
-    \?)		# unknown flag
-        echo >&2 \
-          "usage: $0 [-s] [-v version]"
-          
-        exit 1;;
+    \?)   # unknown flag
+      echo >&2 \
+      "usage: $0 [-s] [-v version]"
+      exit 1;;
   esac
 done
 shift `expr $OPTIND - 1`
 
 machine=$(echo -e `uname -m`)
 
+# Retrieve Platform and Platform Version
 if [ -f "/etc/lsb-release" ];
 then
-  platform=$(cat /etc/lsb-release | grep DISTRIB_ID | cut -d "=" -f 2 | tr '[A-Z]' '[a-z]')
-  platform_version=$(cat /etc/lsb-release | grep DISTRIB_RELEASE | cut -d "=" -f 2)
+  platform=$(grep DISTRIB_ID /etc/lsb-release | cut -d "=" -f 2 | tr '[A-Z]' '[a-z]')
+  platform_version=$(grep DISTRIB_RELEASE /etc/lsb-release | cut -d "=" -f 2)
 elif [ -f "/etc/debian_version" ];
 then
   platform="debian"
   platform_version=$(echo -e `cat /etc/debian_version`)
 elif [ -f "/etc/redhat-release" ];
 then
-  platform=$(echo -e `cat /etc/redhat-release` | perl -pi -e 's/^(.+) release.+/$1/' | tr '[A-Z]' '[a-z]')
+  platform=$(sed 's/^\(.\+\) release.*/\1/' /etc/redhat-release | tr '[A-Z]' '[a-z]')
+  platform_version=$(sed 's/^.\+ release \([.0-9]\+\).*/\1/' /etc/redhat-release)
   if [ "$platform" = "redhat enterprise linux server" ];
   then
     platform="el"
   elif [ "$platform" = "centos" ];
   then
     platform="el"
+  elif [ "$platform" = "centos linux" ];
+  then
+    platform="el"
   elif [ "$platform" = "scientific linux" ];
   then
     platform="el"
   fi
+elif [ -f "/etc/system-release" ];
+then
+  platform=$(sed 's/^\(.\+\) release.\+/\1/' /etc/system-release | tr '[A-Z]' '[a-z]')
+  platform_version=$(sed 's/^.\+ release \([.0-9]\+\).*/\1/' /etc/system-release | tr '[A-Z]' '[a-z]')
+fi
 
-  platform_version=$(echo -e `cat /etc/redhat-release` | perl -pi -e 's/^.+ release ([\d\.]+).*/$1/' | tr '[A-Z]' '[a-z]')
-
-  if [ "$platform" = "el" ];
-  then
-    major_version=$(echo $platform_version | perl -pi -e 's/^(\d).+$/$1/g')
-    case $platform in
+# Mangle $platform_version to pull the correct build
+# for various platforms
+major_version=$(echo $platform_version | cut -d. -f1)
+case $platform in
+  "el")
+    case $major_version in
       "5") platform_version="5.6" ;;
       "6") platform_version="6.0" ;;
     esac
-  fi
-elif [ -f "/etc/system-release" ];
-then
-  platform=$(echo -e `cat /etc/system-release` | perl -pi -e 's/^(.+) release.+/$1/' | tr '[A-Z]' '[a-z]')
-  platform_version=$(echo -e `cat /etc/system-release` | perl -pi -e 's/^.+ release ([\d\.]+).*/$1/' | tr '[A-Z]' '[a-z]')
-fi
+    ;;
+  "debian")
+    case $major_version in
+      "5") platform_version="6.0.1";;
+      "6") platform_version="6.0.1";;
+    esac
+    ;;
+  "ubuntu")
+    case $platform_version in
+      "10.10") platform_version="10.04";;
+      "11.10") platform_version="11.04";;
+    esac
+    ;;
+esac
 
 if [ -z "$version" ];
 then
-  version=$release_version
+    version=$release_version
 fi
 
 if [ $use_shell = 1 ];
@@ -109,19 +126,17 @@ else
   esac
 fi
 
-echo "Downloading Chef $version for $platform $platform_version ..."
+echo "Downloading Chef $version for ${platform}..."
 
-if exists wget; 
+if exists wget;
 then
   wget -O /tmp/$filename http://s3.amazonaws.com/opscode-full-stack/$platform-$platform_version-$machine/$filename
+elif exists curl;
+then
+  curl http://s3.amazonaws.com/opscode-full-stack/$platform-$platform_version-$machine/$filename > /tmp/$filename
 else
-  if exists curl;
-  then
-    curl http://s3.amazonaws.com/opscode-full-stack/$platform-$platform_version-$machine/$filename > /tmp/$filename
-  else
-    echo "Cannot find wget or curl - cannot install Chef!"
-    exit 5
-  fi
+  echo "Cannot find wget or curl - cannot install Chef!"
+  exit 5
 fi
 
 echo "Installing Chef $version"
