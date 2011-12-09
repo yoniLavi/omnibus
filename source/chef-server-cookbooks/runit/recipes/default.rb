e4514efd106b4925ac4d8c1dd901cd6c3510e7f7
Adam Jacob <adam@opscode.com>
2011-12-09 06:44:25 -0800

    Adding sysvinit support

--- source/chef-server-cookbooks/runit/recipes/default.rb
+++ source/chef-server-cookbooks/runit/recipes/default.rb
@@ -38,4 +38,15 @@ when "ubuntu"
   execute "service opscode-runsvdir start" do
     only_if "initctl status opscode-runsvdir | grep stop"
   end
+else
+  # We assume you are sysvinit
+  svdir_line = 'SV:123456:respawn:/opt/opscode/embedded/bin/runsvdir-start'
+  execute "echo '#{svdir_line}' >> /etc/inittab" do
+    not_if "grep '#{svdir_line}' /etc/inittab"
+    notifies :run, "execute[init q]", :immediately
+  end
+
+  execute "init q" do
+    action :nothing
+  end
 end
