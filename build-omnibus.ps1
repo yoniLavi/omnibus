c1a463b9150b06e6c3026f1f35272fd2b2a85828
Bryan McLellan <btm@opscode.com>
2012-09-10 15:29:35 -0700

    Bump for windows 10.14.2 release

--- build-omnibus.ps1
+++ build-omnibus.ps1
@@ -22,7 +22,7 @@ $json_attribs = @"
   },
   "omnibus": {
     "$project_name": {
-      "version": "10.14.0",
+      "version": "10.14.2",
       "iteration": "1",
       "bucket_name": "$bucket_name"
     }
