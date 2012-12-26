f9a5042d33edf40c59f4a9ed09c88d881693912f
Bryan McLellan <btm@opscode.com>
2012-12-26 14:43:54 -0800

    Omnibus 10.16.4-1

--- build-omnibus.ps1
+++ build-omnibus.ps1
@@ -22,7 +22,7 @@ $json_attribs = @"
   },
   "omnibus": {
     "$project_name": {
-      "version": "10.16.2",
+      "version": "10.16.4",
       "iteration": "1",
       "bucket_name": "$bucket_name"
     }
